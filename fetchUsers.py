672288057fced672ff9fc64e826178d22d869f60
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-09-16 21:47:04 +0200

    More traces in fetchUsers.py

--- fetchUsers.py
+++ fetchUsers.py
@@ -14,48 +14,53 @@ import time
 
 from nonblock import bgread
 
-users=set(sys.argv[1:])
+users = set(sys.argv[1:])
 for user in users:
     subprocess.run('./addUser.js {}'.format(user), shell=True)
 
-processes={}
+processes = {}
 for user in users:
-    popen=subprocess.Popen('./fetchUserDetailsAndContribs.js {} --nospin'.format(user),
-                           shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    processes[user]={
+    popen = subprocess.Popen('./fetchUserDetailsAndContribs.js {} --nospin'.format(user),
+                             shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    processes[user] = {
         'popen': popen,
-        'stdout': bgread(popen.stdout)
+        'stdout': bgread(popen.stdout),
+        'exitcode': None,
     }
 
-all_processes_succeeded=True
 while True:
     time.sleep(5)
     print('----')
 
-    all_processes_are_done=True
+    all_processes_are_done = True
     for user, process in processes.items():
         def print_last_stdout_line(user, line):
             print('[{}] {}'.format(user, line))
 
-        exitcode=process['popen'].poll()
+        exitcode = process['popen'].poll()
         if exitcode != None:
             print_last_stdout_line(user, 'exited with {}'.format(exitcode))
-            if exitcode != 0:
-                all_processes_succeeded=False
+            process['exitcode'] = exitcode
             continue
 
-        all_processes_are_done=False
+        all_processes_are_done = False
 
         # we print the second-to-last line of stdout because often the very last one is
         # incomplete/unflushed:
-        stdout=process['stdout'].data if process['stdout'].data else b''
-        stdout=stdout.decode().split('\n')
+        stdout = process['stdout'].data if process['stdout'].data else b''
+        stdout = stdout.decode().split('\n')
         if len(stdout) < 2:
-            stdout=['just started', '']
+            stdout = ['just started', '']
         print_last_stdout_line(user, stdout[-2])
 
     if all_processes_are_done:
         break
 
-if not all_processes_succeeded:
-    raise ChildProcessError('A process failed')
+for user, process in processes.items():
+    exitcode = process['exitcode']
+    if exitcode != 0:
+        print('----')
+        errmsg = '{} failed with {}'.format(user, exitcode)
+        print('{}:'.format(errmsg))
+        print(process['stdout'].data.decode())
+        raise ChildProcessError(errmsg)
