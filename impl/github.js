461575bcea31e3ac93c39703e4bc197bfe805929
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-09-17 14:39:01 +0200

    Trace for understanding #2

--- impl/github.js
+++ impl/github.js
@@ -32,13 +32,20 @@
   const fetchGHRateLimit = async oraSpinner => {
     const ghUrl = `https://api.github.com/rate_limit`;
     const ghDataJson = await fetchJson(authify(ghUrl), oraSpinner);
-    return ghDataJson.resources.core;
+    return ghDataJson.resources;
   };
 
   const fetchGHJson = async (url, oraSpinner, acceptedErrorCodes=[],
                              /*Date*/ifModifiedSince) => {
-    await waitForRateLimit(oraSpinner);
-    return await fetchJson(authify(url), oraSpinner, acceptedErrorCodes, ifModifiedSince);
+    const rateLimit = await waitForRateLimit(oraSpinner);
+    try {
+      return await fetchJson(authify(url), oraSpinner, acceptedErrorCodes, ifModifiedSince);
+    } catch (e) {
+      console.error(`Error while fetching ${url}`);
+      console.error('API rate limit state:');
+      console.error(rateLimit);
+      throw e;
+    }
   };
 
   module.exports = {
@@ -46,18 +53,22 @@
     fetchGHRateLimit,
   };
 
+  // Waits until we are far away from hitting GitHub's API rate limit.
+  // Returns GitHub's rate limit object for reference.
   async function waitForRateLimit(oraSpinner) {
     const oldSpinnerText = oraSpinner.text;
-    const rateLimit = await fetchGHRateLimit(oraSpinner);
-    if (rateLimit.remaining <= 10) {
+    let rateLimit = await fetchGHRateLimit(oraSpinner);
+    if (rateLimit.core.remaining <= 10) {
       const now = (new Date).getTime() / 1000;
-      const secondsToSleep = Math.ceil(rateLimit.reset - now);
+      const secondsToSleep = Math.ceil(rateLimit.core.reset - now);
       if (secondsToSleep >= 0) {
         oraSpinner.text += ` (waiting ${secondsToSleep} second(s) for API rate limit)`;
         await sleep(secondsToSleep * 1000);
         oraSpinner.text = oldSpinnerText;
+        rateLimit = await fetchGHRateLimit(oraSpinner);
       }
     }
+    return rateLimit;
   }
 
 })();
