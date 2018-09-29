718a91067f527e88bac5c22f7e34f2aa3339eafe
wowawiwa <charles@pubnative.net>
2018-09-29 11:59:06 +0200

    Allow defining DB directory as env var

--- impl/data.js
+++ impl/data.js
@@ -2,17 +2,23 @@
 'use strict';
 
 (() => {
-
   const os = require('os');
+  const fs = require('fs');
   const path = require('path');
+  const dbPath = process.env.DBDIR || path.join(os.homedir(), 'data');
+
+  if (!fs.existsSync(dbPath)) {
+    throw `${dbPath} directory doesn't exist`
+  }
 
   module.exports = {
-    root: path.join(os.homedir(), 'data')
+    root: dbPath
   };
   module.exports.users = path.join(module.exports.root, 'users');
   module.exports.contribs = path.join(module.exports.root, 'contribs');
   module.exports.repos = path.join(module.exports.root, 'repos');
   module.exports.repoCommits = path.join(module.exports.root, 'repoCommits');
+
   module.exports.orgs = path.join(module.exports.root, 'orgs.json');
   module.exports.nonOrgs = path.join(module.exports.root, 'nonOrgs.json');
   module.exports.meta = path.join(module.exports.root, 'meta.json');
