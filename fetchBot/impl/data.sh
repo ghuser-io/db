ccdb8bab2e1f804f00f25843350ce9486cab2790
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-10-07 13:28:11 +0200

    Stop backing up to EFS.

--- fetchBot/impl/data.sh
+++ fetchBot/impl/data.sh
@@ -4,7 +4,6 @@ source "$(dirname $BASH_SOURCE)/utils.sh"
 
 DATA_ON_EBS=~/data
 DATA_ON_EFS=~/efs/data.git
-BACKUP_ON_EFS=~/efs/data
 
 function initData {
   if [[ ! -d "$DATA_ON_EFS" ]]; then
@@ -16,27 +15,19 @@ function initData {
 }
 
 function backupAndPublishToS3 {
-  trace "Backing up on EFS..."
-  if [[ ! -d "$BACKUP_ON_EFS" ]]; then
-    git clone "$DATA_ON_EFS" "$BACKUP_ON_EFS"
-  fi
-  pushd "$BACKUP_ON_EFS"
-  git pull
-  popd
-
   publishToS3
   backupToS3
 }
 
 function publishToS3 {
   trace "Publishing to S3..."
-  time aws s3 sync "$BACKUP_ON_EFS" s3://ghuser/data --exclude ".git/*"
+  time aws s3 sync "$DATA_ON_EBS" s3://ghuser/data --exclude ".git/*"
 }
 
 function backupToS3 {
   trace "Backing up on S3..."
   rm -rf /tmp/backups
   mkdir /tmp/backups
-  tar pczvf "/tmp/backups/$(date -u +%Y-%m-%d).tar.gz" --exclude .git -C "$BACKUP_ON_EFS" .
+  tar pczvf "/tmp/backups/$(date -u +%Y-%m-%d).tar.gz" --exclude .git -C "$DATA_ON_EBS" .
   time aws s3 sync /tmp/backups s3://ghuser/backups
 }
